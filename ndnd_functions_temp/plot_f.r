0ff69ad03ebf7602f855f83c59e2a29a6d30b57b
ElliotSivel <elliotsivel002@hotmail.fr>
2019-04-01 15:14:37 +0200

    Moving unfinished plot function to temporary function files

--- ndnd_functions_temp/plot_f.r
+++ ndnd_functions_temp/plot_f.r
@@ -1,13 +1,6 @@
-BiomassSeries_v<-as.vector(BiomassSeries)
-Species_plot<-rep(NDNDData$Species,byrow=F,each=NDNDData$Tmax)
-Tmax_v<-as.numeric(rep(1:NDNDData$Tmax,by=NDNDData$ns))
-Biomass_plot<-as.data.frame(cbind(Tmax_v,BiomassSeries_v,Species_plot))
-
-
-
 library(ggplot2)
 library(gridExtra)
-ggplot(Biomass_plot,aes(x=Tmax_v,y=BiomassSeries_v))+geom_line()+facet_grid(BiomassSeries_v~Species_plot)
+ggplot(as.data.frame(BiomassSeries[,1]))+aes(1:NDNDData$Tmax,BiomassSeries[,1])+geom_line()
 
 grid.arrange(ggplot(as.data.frame(BiomassSeries[,1]))+aes(1:NDNDData$Tmax,BiomassSeries[,1])+geom_line()+xlab("Years")+ylab(expression(Biomass (tons/ ~ km^2 )))+ggtitle(NDNDData$Speciesnames[1]),
              ggplot(as.data.frame(BiomassSeries[,2]))+aes(1:NDNDData$Tmax,BiomassSeries[,2])+geom_line()+xlab("Years")+ylab("")+ggtitle(NDNDData$Speciesnames[2]),
