28be204717dccd2ff70c7badd428aff0b21ab540
ElliotSivel <elliotsivel002@hotmail.fr>
2019-05-15 15:24:31 +0200

    Comments added sys.setenv externalized added Yes No question for loading NDNDData file or run dataprep code

--- ndnd_code/NDND_main.r
+++ ndnd_code/NDND_main.r
@@ -3,9 +3,23 @@
 #### Model simulation
 #### Version v1.1.5
 #### 08.04.19
-#### Author : Elliot Sivel
+#### Author : Elliot Sivel, Benjamin Planque and Ulf Lindstrøm
 #### License ???
 ###############################################################################################
+
+
+# 0. Warning --------------------------------------------------------------
+
+# The code needs to run parts in C++ to increase the speed of simulation
+# Therefore, the some functions are coded in c++ and need to be compiled
+# If you are working with Linux or Mac, the compiler is included, you don't need to include any other package
+# If you are working on Windows, Rtools needs to be downloaded and installed on the computer
+# https://cran.r-project.org/bin/windows/Rtools/
+# Once installed you need to add the the path to Rtools to the environment path with the function sys.setenv()
+
+#####
+# Needs to be generalized
+Sys.setenv(PATH = "C:/Users/a22073/Documents/Rtools/bin/;C:/Users/a2207/Documents/R-3.6.0/bin/x64;C:/Program Files(x86)/Common Files/Oracle/Java/javapath;C:/WINDOWS/system32;C:/WINDOWS; C:/WINDOWS/System32/Wbem;C:/WINDOWS/System32/WindowsPowerShell/v1.0/;C:/Program Files/Intel/WiFi/bin/;C:/Program Files/Common Files/Intel/WirelessCommon/;C:/Program Files/Git/cmd;C:/Users/Administrator/AppData/Local/Microsoft/WindowsApps;C:/Users/Administrator/AppData/Local/Box/Box Edit/;")
 
 # 1. Initialization ----------------------------------------------------------
 
@@ -17,53 +31,58 @@ rm(list=ls())                               # clear the work environment
 
 # Load specific libraries
 
-library(rstudioapi)                                    # Loading the library for directory and file interactive choice
-library(LIM)                                           # Loading the library LIM -- The LIM library is used for the sampling of flows -- Soetaert and van Oevelen (2014)
-library(ggplot2)                                       # Loading the ggplot package -- graphics package 
-library(tidyverse)
-library(pracma)
-library(linprog)
-library(RcppEigen)
-library(Rcpp)
-library(inline)
-library(devtools)
-library(memisc)
-
-# Set the environement of the system
-
-Sys.setenv(PATH = "C:/Users/a22073/Documents/Rtools/;C:/Users/a2207/Documents/R-3.6.0/bin/x64;C:/Program Files(x86)/Common Files/Oracle/Java/javapath;C:/WINDOWS/system32;C:/WINDOWS; C:/WINDOWS/System32/Wbem;C:/WINDOWS/System32/WindowsPowerShell/v1.0/;C:/Program Files/Intel/WiFi/bin/;C:/Program Files/Common Files/Intel/WirelessCommon/;C:/Program Files/Git/cmd;C:/Users/Administrator/AppData/Local/Microsoft/WindowsApps;C:/Users/Administrator/AppData/Local/Box/Box Edit/;")
+library(rstudioapi)                         # Loading the rstudioapi package -- Directory and file interactive choice
+library(LIM)                                # Loading the LIM package -- The LIM library is used for the sampling of flows -- Soetaert and van Oevelen (2014)
+library(ggplot2)                            # Loading the ggplot package -- Graphics package 
+library(tidyverse)                          # Loading the tidyverse package -- Set of packages for data manipulations
+library(pracma)                             # Loading the pracma package -- Mathematical tools and functions
+library(linprog)                            # Loading the linprog package -- Linear programming and optimization
+library(RcppEigen)                          # Loading the RcppEigen package -- Linear algebra on C++ integrated in R
+library(Rcpp)                               # Loading the Rcpp package -- Integration of R and C++
+library(inline)                             # Loading the inline package -- Read C++ functions called in R
+library(devtools)                           # Loading the devtools package -- Tools for package development
+library(memisc)                             # Loading the memisc package -- 
+library(plyr)                               # Loading the plyr package -- Dataframe manipulation
+library(abind)                              # Loading the abind package -- Bindind arrays 
 
 # 2. Load functions -------------------------------------------------------
 
-setwd(selectDirectory(caption = "Select your work directory : "))
-wd<-getwd()
+setwd(selectDirectory(caption = "Select your work directory : "))                  # Chose your initial work directory
+wd<-getwd()                                                                        # Save initial work directory
 
-NDNDfunctions<-list.files(paste(wd,"/ndnd_functions",sep=""))
+NDNDfunctions<-list.files(paste(wd,"/ndnd_functions",sep=""))                      # List the functions in the folder ndnd_functions
+# Source the functions in the folder ndnd_functions
 for (i in 1:length(NDNDfunctions)){
   function2source<-paste(wd,"/ndnd_functions","/",NDNDfunctions[i],sep="")
   source(function2source)
 }
 
 # 3. Load directories -------------------------------------------------
-## source functions
 
-dir_tab<-directories(wd=wd)
+dir_tab<-directories(wd=wd)                    # Save all directories available from the initial directory in a vector
 
-# 4a. prepare data file ---------------------------------------------------
+# 4. prepare data file ---------------------------------------------------
 
-source('./ndnd_code/Dataprep.r')
+# The Data file is not varying from unless the initial data has been changed
+# If a NDNDData file has already been saved for a precise configuration you don't need to rerun the entire dataprep
 
-# 4b. load data file --------------------------------------------------------------
+# Question :
+test_ndnd_data<-askYesNo("Do you have already a NDNDData file for the configuration setups you wish?")
 
-load(file="NDNDData.Rdata")
+# If you have the NDNDData file you can directly load it
+if(test_ndnd_data==F){
+  source('./ndnd_code/Dataprep.r')
+} else {load(file=paste(dir_tab$data_dir,"/NDNDData.RData",sep=""))}
 
 # 5. run the simulation ------------------------------------------------------
 
-T1<-Sys.time()
-NDNDSimulation=SimNDND(NDNDData)
-T2<-Sys.time()
+# Here it run a the simulation for one simulation
 
-Tdif<-difftime(T2,T1)
+T1<-Sys.time()                                       # Timer on
+NDNDSimulation=SimNDND(NDNDData)
+T2<-Sys.time()                                       # Timer off
+
+Tdif<-difftime(T2,T1)                                # Simulation time
 Tdif
 
 # 6. save simulation outputs  -------------------------------------------
