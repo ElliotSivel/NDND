2e8cc4625d76468d524cea347bedb1ef00494024
unknown <Administrator@hi-10906.ad.hi.no>
2020-12-14 13:31:37 +0100

    Compute the A from Ax<=b. Is constant for entire simulation.

--- ndnd_functions/NDND_computeA_f.r
+++ ndnd_functions/NDND_computeA_f.r
@@ -31,27 +31,23 @@ ComputeA<-function(NDNDData){
   # Gama(i)*sum(Fji*Kapa(i))-sum(Fij)
   
   # Vector VI --> the possible flows towards predators for each species
-  
-  VI<-as.matrix(rep(1:ns,byrow=F,each=ns))       # Vector from 1 to ns
+  VI<-as.matrix(rep(1:ns,byrow=F,each=ns))      # Vector from 1 to ns
                                                 # Replication each value ns times
                                                 # Vector dimension 1:nn
   
   # Vector VJ --> The possible flows incoming for each species
-  
-  VJ<-as.matrix(rep(1:ns,times=ns))              # Vector from 1 to ns
+  VJ<-as.matrix(rep(1:ns,times=ns))             # Vector from 1 to ns
                                                 # Replication of the vector ns times
                                                 # Vector of dimension 1:nn
   
   # Compute Fij matrix --> SumFij
   # Matrix of flows outgoing from the species i to the species j
-
   Fij<-c%*%e==d%*%t(VI)                         # Matrix product of c-e and d-VI
                                                 # Fij is the matrix were the two computed matricial product are equal
   SumFij<-apply(Fij,c(1,2),as.numeric)          # Transformation from bolean to numerical values
   
   # Compute Fji matrix --> SumFji
   # Matrix of flows incoming species i from species j 
-
   Fji<-c%*%e==d%*%t(VJ)                         # Matrix product of c-e and d-VJ
                                                 # Fji is the matrix were the two computed matricial product are equal
   SumFji<-apply(Fji,c(1,2),as.numeric)          # Transformation from bolean to numerical values
@@ -59,40 +55,32 @@ ComputeA<-function(NDNDData){
   # Compute Kapa matrix
   # For all parameters, values are in vectors
   # for matricial computation, we need to replicate the Kapa values --> Kapa2 
-
   Kapa2<-matrix(rep(as.numeric(Kapa), each = ns), nrow = ns)             # Replication of the kapa vector in rows ns times
   Kapa2<-t(apply(Kapa2,1,rep,each=ns,byrow=TRUE))                        # Replication of the columns of Kapa2 ns times
 
   # Compute SumKiFji matrix
   # (Vectorized) Matrix of flows from the species j to the species i -- predation flows 
-  
   SumKjFji<-SumFji*Kapa2                          # Multiplying each flow of the SumFji matrix to the corresponding value of Kapa
  
   # Implementing constraints on flows
   # There are 5 constraints -- A1,A2,A3,A4,A5
   
   # First constraint : Biomass are bounded below - Refuge Biomass (Beta)
-  
   A1<-SumFij-(Gama%*%e)*SumKjFji
   
   # Second constraint : Biomass increases are bounded above - Inertia (Rho)
-  
   A2=-A1
   
   # Third constraint : Flows are bounded above - satiation (Sigma)
-  
   A3<-SumFji
  
   # Fourth constraint : Flows are positive
-  
   A4<--diag(nn)
  
   # Fifth constraint : Biomass decreases are bounded below (-(Rho))
-  
   A5=A1
   
   # Create an object where all constraints matrix are just bound --> A
-  
   A<-rbind(A1,A2,A3,A4,A5)
   
   return(A)
